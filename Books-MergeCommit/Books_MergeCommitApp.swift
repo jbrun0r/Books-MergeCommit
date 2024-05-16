e40707ac58e2c7ce0b2b92b53d93e97fe7f97b89
João Bruno <joaobruno.rf@gmail.com>
2024-05-16 16:13:46 -0300

    feat: Create model Book

--- Books-MergeCommit/Books_MergeCommitApp.swift
+++ Books-MergeCommit/Books_MergeCommitApp.swift
@@ -9,24 +9,12 @@ import SwiftUI
 import SwiftData
 
 @main
-struct Books_MergeCommitApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
+struct Books_RebaseAndMergeApp: App {
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(sharedModelContainer)
+        .modelContainer(for: Book.self)
     }
 }
---
+++ Books-MergeCommit/Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Extensions.swift
+//  Books-MergeCommit
+//
+//  Created by João Bruno Rodrigues on 16/05/24.
+//
+
+import Foundation
+
+extension String {
+    static let empty = ""
+}
